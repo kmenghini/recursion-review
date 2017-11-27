594ad90da633533ffcbbabab9bb2713d2eea79f2
kmenghini <kaitlyn.menghini@gmail.com>
2017-11-27 12:34:33 -0800

    Add current stringifyJSON code
    Adding current code.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,6 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-  // string, number, boolean, function, object  
+  // string, number, boolean, function, object
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === undefined) {
+    return 'null';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  // if (!obj) {
+  //   return 'false';
+  // }
+  if (typeof obj === 'number') {
+    return obj + '';
+  }
+  if (typeof obj === 'boolean') {
+    return obj + '';
+  }
+  if (Array.isArray(obj)) {
+    var collection = '';
+    for (var i = 0; i < obj.length; i++) {
+      collection += stringifyJSON(obj[i]) + ','; //1234 -> 1, 2, 3, 4,
+    }
+    return '[' + collection.slice(0, collection.length - 1) + ']';
+  }
+  if (typeof obj === 'object') {
+    var collection = '';
+    if (typeof obj !== 'null') {
+      for (var key in obj) {
+        if (typeof obj[key] !== 'function' && obj[key] !== 'undefined') {
+          collection += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+        }
+        
+      }
+    }
+    return '{' + collection.slice(0, collection.length - 1) + '}';
+  }
+if (typeof obj === 'function' ) { return obj + ''; }
   return obj + '';
+
 };
+
+console.log(stringifyJSON({'boolean, true': true, 'boolean, false': false, 'null': null }));
+
+stringifiableObjects.forEach(function(test) {
+      var expected = JSON.stringify(test);
+      console.log(expected);
+      var result = stringifyJSON(test);
+      console.log(result);
+      expect(result).to.equal(expected);
+    });
