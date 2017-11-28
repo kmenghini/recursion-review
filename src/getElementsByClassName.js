cfc872cd7187df5bdaaf468b2c62a6206be1380d
kmenghini <kaitlyn.menghini@gmail.com>
2017-11-27 21:29:10 -0800

    Add fully functioning getElementsByClassName
    This program satisfies the test in the Mocha test suite.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, element) {
   // your code here
+  element = element || document.body;
+
+  var results = [];
+
+  if (element.classList && element.classList.contains(className)) {
+    results.push(element);
+  }
+  if (element.childNodes) {
+    for (var i = 0; i < element.childNodes.length; i++) {
+      results = results.concat(getElementsByClassName(className, element.childNodes[i]));
+    }
+  }
+  return results;
 };
